44c37e499f6f23125e6fae0440d8b874277b02dc
fcaprile <fcaprile@gmail.com>
2019-11-14 19:20:00 -0300

    prop errores

--- scripts/sonda langmuir doble/calcular densidad.py
+++ scripts/sonda langmuir doble/calcular densidad.py
@@ -5,19 +5,40 @@ Created on Wed Aug 14 10:44:59 2019
 @author: DG
 """
 import numpy as np
-Ii0=0.087
-Te=4.6*1.6*10**-19
+from uncertainties import ufloat
+
+e=1.6*10**-19
+
+#Ii0=ufloat(0.087)
+
+'''
+valores para cerca sin iman
+Ii0=ufloat(0.2325,0.0025) 
+Te=ufloat(4.6*e,1*e)
+'''
+
+'''
+valores para 3J (L6)
+Ii0=ufloat(0.0006,0.000002) 
+Te=ufloat(4.0*e,1.3*e)
+'''
+
+'''
+valores para 4.5J (L6)
+Ii0=ufloat(0.002,0.0001) 
+Te=ufloat(6*e,1*e)
+'''
+
+
 mn=1.67*10**-27
 mf=19*mn
 mc=12*mn
-e=1.6*10**-19
 
 r=0.0005
 h=0.005
 A=2*np.pi*r*h+np.pi*r**2
 
-v=np.sqrt(Te/mc)/3+2*np.sqrt(Te/mf)/3
+v=(Te/mc)**0.5/3+2*(Te/mf)**0.5/3
 ne=Ii0/0.61/e/A/v
 
 print(ne)
-    
